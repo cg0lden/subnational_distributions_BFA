205ea4cfcfdada26b902db791321696cc5d1feff
Simone Amanda Passarelli <Simone@Simones-MacBook-Air.local>
2020-11-13 15:51:37 -0500

    Updated code for a2z dataset to run but it is still missing the age variable so could not be fully cleaned

--- scripts/uganda_A2Z.R
+++ scripts/uganda_A2Z.R
@@ -1,18 +1,17 @@
-# SPADE for estimating distributions
-# Uganda Harvest Plus dataset
+# Prep dataset for SPADE
+# Uganda A27 dataset
 
 library(tidyverse)
 library(haven)
 library(here)
-library(SPADE.RIVMNwCore)
 
 # Load the Zambia data from the Stata file
 
-uganda_h <- read_dta(here("data", "Uganda-A2Z fg24.dta"))
+uganda_a <- read_dta(here("data", "raw", "Uganda-A2Z fg24.dta"))
 
 
 # Limit to variables needed for analysis
-uganda_h_nut <- uganda_h %>%
+uganda_a_nut <- uganda_a %>%
   group_by(hid, pid, Recall) %>%
   summarize(sum_b12 = sum(VIT_B12_MCG),
             sum_iron = sum(IRON_MG),
@@ -21,12 +20,12 @@ uganda_h_nut <- uganda_h %>%
   )
 
 # Merge in identifiers, incl age/sex
-uganda_h_merge<-uganda_h %>%
+uganda_a_merge<-uganda_a %>%
   select(agey, sex, hid, pid, Recall) %>%
   distinct() # drop duplicates (since there is one observation per food item)
 
 # Rename and format variables for spade
-uganda_h_nut_spade <- uganda_h_nut %>% left_join(uganda_h_merge, by=c("hid", "pid", "Recall")) %>%
+uganda_a_nut_spade <- uganda_a_nut %>% left_join(uganda_a_merge, by=c("hid", "pid", "Recall")) %>%
   rename(age=agey, b12=sum_b12, sum_iron = iron, sum_zinc = zinc, sum_vita=vita) %>%
   mutate(hid=as.character(hid),
          pid=as.character(pid)) %>%
@@ -35,7 +34,7 @@ uganda_h_nut_spade <- uganda_h_nut %>% left_join(uganda_h_merge, by=c("hid", "pi
   ungroup() %>%
   select(id, age, sex, mday, b12, iron, zinc, vita)
 
-uganda_h_nut_spade$id <- as.integer(uganda_h_nut_spade$id)
+uganda_a_nut_spade$id <- as.integer(uganda_a_nut_spade$id)
 
 # 5 1-4 years
 #6 5-9 years
@@ -56,10 +55,10 @@ uganda_h_nut_spade$id <- as.integer(uganda_h_nut_spade$id)
 #21 80 plus
 
 # Make separate datasets for men and women
-f.spade_wom <- uganda_h_nut_spade %>%
+f.spade_wom <- uganda_a_nut_spade %>%
   filter(sex==2)
 
-f.spade_men <- uganda_h_nut_spade %>%
+f.spade_men <- uganda_a_nut_spade %>%
   filter(sex==1)
 
 # Running Spade
@@ -72,8 +71,8 @@ f.spade(frml.ia=b12~fp(age),
         min.age=1,
         max.age=69,
         sex.lab="female",
-        output.name = "b12_uganda_h",
-        spade.output.path = here("results", "uganda_h_women"),
+        output.name = "b12_uganda_a",
+        spade.output.path = here("results", "uganda_a_women"),
         age.classes=c(1-4, 5-9, 10-14, 15-19, 20-24, 25-29, 30-34, 35-39, 40-44, 45-49, 50-54, 55-59, 60-64, 65-70)
 )
 
