6aae111ea75e462f24a1ad5ec53a4f8ca6d3e7f4
Simone Passarelli <simoneapassarelli@gmail.com>
2021-02-22 11:42:37 -0500

    Updated the italy data file to include all available nutrients

--- scripts/Subnational distributions scripts/italy.R
+++ scripts/Subnational distributions scripts/italy.R
@@ -8,9 +8,12 @@ library(janitor)
 # Load the italy data from the Stata file (coded with the food groups)
 
 # Consumption data
-italy <- read_dta(here( "data", "raw", "italy", "italy_omega.dta")) %>% 
-  clean_names() %>% select(subject, survey_day, vita, omega_3, calc, iron, food_amount_reported, ingredient, code_ingredient, vitb12) #no zinc for italy
-
+italy <- read_dta(here("data", "raw", "Italy", "italy_omega.dta")) %>% 
+  clean_names() %>% select(-c(adm0_code, adm0_name, adm1_code, adm1_name, adm2_code,
+                              adm2_name, round, consumption_day, consumption_month,
+                              consumption_year, week_day, exception_day:food_amount_proc, water,
+                              a_prot, v_prot, zinc, na, cu, adsugar:vitk, epa, dha, omega_3_100, merge, b12_100))
+  
 # id data
 italy_id <- read_csv(here( "data", "raw", "italy", "subject_user.csv")) %>% 
   clean_names() %>% select(subject, sex, age_year)
@@ -20,37 +23,22 @@ table(italy$survey_day)
 
 # Need to add snail nutrients
 
-# Red meat=9, processed meat=10
 #removed values where vita > 10000
 italy_nut <-  italy %>% 
   rename(mday = survey_day, id=subject) %>% 
-  mutate(red_meat = case_when(code_ingredient==50001 ~ food_amount_reported,
-                              code_ingredient==50002 ~ food_amount_reported,
-                              code_ingredient==50003 ~ food_amount_reported,
-                              code_ingredient==50004 ~ food_amount_reported,
-                              code_ingredient >= 50012 & code_ingredient <=50063 ~ food_amount_reported,
-                              code_ingredient >= 50088 & code_ingredient <=50112 ~ food_amount_reported,
-                              TRUE ~ 0)) %>% 
   group_by(id, mday) %>%
-  summarize(b12 = sum(vitb12),
-            iron = sum(iron),
-            vita = sum(vita),
-            calc = sum(calc),
-            red_meat = sum(red_meat),
-            omega_3 = sum(omega_3)) %>% distinct()
+  replace(is.na(.), 0) %>% 
+  summarize_at(vars(energy:omega_3), sum) %>% distinct()
+
 
 # Merge in identifiers, incl age/sex
 italy_merge <- italy_id %>%
   rename( age=age_year, id=subject) %>%
   dplyr::select( age, sex,  id) %>% group_by(id) %>% distinct()
 
-
-# italy_merge_2 <-  merge(italy_merge, italy_weights, by.all="id", all.x=T)
-
 # Rename and format variables for spade
 italy_spade <- italy_nut %>% 
   left_join(italy_merge, by=c("id")) %>%
-  dplyr::select(id, age, sex, mday, b12, iron, vita, calc, red_meat,  omega_3) %>% 
   mutate(id=as.integer(id)) %>% distinct()
 
 
@@ -71,7 +59,7 @@ italy_missings
 #   }
 # }
 
-save(italy_spade, file=here("data", "processed", "italy"), replace)   
+save(italy_spade, file=here("data", "processed", "Subnational distributions", "italy"), replace)   
 
 
 
